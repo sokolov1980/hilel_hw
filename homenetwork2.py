4c7fedc7a7abc98cbe477ac21c65360d4ea28144
sokolov1980 <falcon_2005@ukr.net>
2023-12-15 21:40:38 +0200

    implemented second task

--- homenetwork2.py
+++ homenetwork2.py
@@ -54,3 +54,18 @@ elif n3 == n2 == n1:
 result = (n1 + n2 + n3) / 3
 print("average: ", end=" ")
 print(result)
+
+#Користувач вводить з клавіатури кількість метрів.
+number = int(input("Enter number of meters: "))
+#выводим мили
+result1 = number * 0.000621371
+print("mile: ", end=" ")
+print(result1)
+#выводим дюймы
+result2 = number * 39.3701
+print("inch: ", end=" ")
+print(result2)
+#выводим ярды
+result3 = number * 1.09361
+print("yard: ", end=" ")
+print(result3)
