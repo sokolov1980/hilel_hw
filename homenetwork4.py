215384f21d1743783d2130b81e2b7d113f5543d0
sokolov1980 <falcon_2005@ukr.net>
2023-12-21 23:37:05 +0200

    implemented task 2

--- homenetwork4.py
+++ homenetwork4.py
@@ -1,16 +1,27 @@
 #Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку. Виведіть обидві кількості на екран
+# try:
+#     line = input("Enter the line: ")
+#     dig = 0
+#     alp = 0
+#     for i in line:
+#         if i.isdigit():
+#             dig += 1
+#         elif i.isalpha():
+#             alp += 1
+#     print(f"Result number: {dig}")
+#     print(f"Result letter: {alp}")
+# except Exception as error:
+#     print(f"Error: {error}")
+
+#Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається символ
 try:
     line = input("Enter the line: ")
-    dig = 0
-    alp = 0
-    for i in line:
-        if i.isdigit():
-            dig +=1
-        elif i.isalpha():
-            alp += 1
-        else:
-            alp += 1
-    print(f"Result number: {dig}")
-    print(f"Result letter: {alp}")
+    sim = input("Enter search symbol: ")
+    qua = 0
+    for i in range(len(line)):
+        if line[i] == sim:
+            qua += 1
+    print(f"Result symbol: {qua}")
 except Exception as error:
     print(f"Error: {error}")
+
