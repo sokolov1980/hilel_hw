acd9f54b66d6e7522404d04b55f45f86930af7d3
sokolov1980 <falcon_2005@ukr.net>
2023-12-22 17:26:09 +0200

    implemented task 4

--- homenetwork4.py
+++ homenetwork4.py
@@ -1,4 +1,4 @@
-#Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку. Виведіть обидві кількості на екран
+##Користувач вводить рядок з клавіатури. Порахуйте кількість літер, цифр у рядку. Виведіть обидві кількості на екран
 # try:
 #     line = input("Enter the line: ")
 #     dig = 0
@@ -13,7 +13,7 @@
 # except Exception as error:
 #     print(f"Error: {error}")
 
-#Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається символ
+##Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається символ
 # try:
 #     line = input("Enter the line: ")
 #     sim = input("Enter search symbol: ")
@@ -25,7 +25,7 @@
 # except Exception as error:
 #     print(f"Error: {error}")
 
-#Користувач вводить з клавіатури рядок, слово для пошуку, слово для заміни. Зробіть у рядку заміну
+##Користувач вводить з клавіатури рядок, слово для пошуку, слово для заміни. Зробіть у рядку заміну
 # try:
 #     text = input("Enter the text: ")
 #     word = input("Enter word to search: ")
@@ -35,41 +35,47 @@
 # except Exception as error:
 #     print(f"Error: {error}")
 
-#Дано рядок. (зробити зрізи)
-try:
-    text = input("Enter the text: ")
-#виведіть третій символ
-    print(text[2:3])
-#виведіть передостанній символ
-    print(text[-2:-1])
-#виведіть перші п'ять символів
-    print(text[0:5])
-#виведіть весь рядок, крім двох останніх символів
-    print(text[0:-2])
-#У п'ятому рядку виведіть усі символи з парними індексами
-    print(text[1::2])
-# У шостому рядку виведіть усі символи з непарними індексами
-    print(text[0::2])
-#У сьомому рядку виведіть усі символи у зворотному порядку.
-    print(text[::-1])
-#У восьмому рядку виведіть усі символи рядка через один у зворотному порядку, починаючи з останнього
-    print(text[::-2])
-#У дев'ятому рядку виведіть довжину цього рядка
-    print(len(text))
-except Exception as error:
-    print(f"Error: {error}")
-
-#Додатково
+# #Дано рядок. (зробити зрізи)
 # try:
-#     text = input("Enter the line: ")
-#     dig = 0
-#     alp = 0
-#     for i in text:
-#         if i.isdigit():
-#             dig += 1
-#         elif i.isalpha():
-#             alp += 1
-#     print(f"Result number: {dig}")
-#     print(f"Result letter: {alp}")
+#     text = input("Enter the text: ")
+# #виведіть третій символ
+#     print(text[2:3])
+# #виведіть передостанній символ
+#     print(text[-2:-1])
+# #виведіть перші п'ять символів
+#     print(text[0:5])
+# #виведіть весь рядок, крім двох останніх символів
+#     print(text[0:-2])
+# #У п'ятому рядку виведіть усі символи з парними індексами
+#     print(text[1::2])
+# # У шостому рядку виведіть усі символи з непарними індексами
+#     print(text[0::2])
+# #У сьомому рядку виведіть усі символи у зворотному порядку.
+#     print(text[::-1])
+# #У восьмому рядку виведіть усі символи рядка через один у зворотному порядку, починаючи з останнього
+#     print(text[::-2])
+# #У дев'ятому рядку виведіть довжину цього рядка
+#     print(len(text))
 # except Exception as error:
 #     print(f"Error: {error}")
+
+#Додатково
+try:
+    text = input("Enter the text: ")
+    dig = 0
+    spa = 0
+    exc = 0
+    for i in text:
+        if i.isdigit():
+            dig += 1
+    for i in range(len(text)):
+        if text[i] == " ":
+            spa += 1
+        elif text[i] == "!":
+            exc += 1
+    print(text.title())
+    print(f"Result number: {dig}")
+    print(f"Result space: {spa}")
+    print(f"Result exclamation point: {exc}")
+except Exception as error:
+    print(f"Error: {error}")
