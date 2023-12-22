ac7ed5775b9f647ab984703d028011ca16d251be
sokolov1980 <falcon_2005@ukr.net>
2023-12-22 14:23:20 +0200

    implemented task 3

--- homenetwork4.py
+++ homenetwork4.py
@@ -14,14 +14,24 @@
 #     print(f"Error: {error}")
 
 #Користувач вводить з клавіатури рядок та символ для пошуку. Порахуйте скільки разів у рядку зустрічається символ
+# try:
+#     line = input("Enter the line: ")
+#     sim = input("Enter search symbol: ")
+#     qua = 0
+#     for i in range(len(line)):
+#         if line[i] == sim:
+#             qua += 1
+#     print(f"Result symbol: {qua}")
+# except Exception as error:
+#     print(f"Error: {error}")
+
+#Користувач вводить з клавіатури рядок, слово для пошуку, слово для заміни. Зробіть у рядку заміну
 try:
-    line = input("Enter the line: ")
-    sim = input("Enter search symbol: ")
-    qua = 0
-    for i in range(len(line)):
-        if line[i] == sim:
-            qua += 1
-    print(f"Result symbol: {qua}")
+    text = input("Enter the text: ")
+    word = input("Enter word to search: ")
+    word_r = input("Enter word to replace: ")
+    text = text.replace(word, word_r )
+    print(f"Modified text: {text}")
 except Exception as error:
     print(f"Error: {error}")
 
