e30d9082650b3b3c34cc1de4cd9e271ad8fb2b37
sokolov1980 <falcon_2005@ukr.net>
2023-12-26 18:47:42 +0200

    implemented task 2

--- homenetwork5.py
+++ homenetwork5.py
@@ -1,85 +1,120 @@
-#1.У списку цілих, заповненому випадковими числами обчислити:
+# #1.У списку цілих, заповненому випадковими числами обчислити:
+# try:
+#     import random
+#     NUMS_SIZE = 10
+#     MIN_NUMBER = -20
+#     MAX_NUMBER = 20
+#     numbers = []
+#     number = []
+#     number2 = []
+#     number3 = []
+#     for i in range(NUMS_SIZE):
+#         numbers.append(random.randint(MIN_NUMBER, MAX_NUMBER))
+#     print(numbers)
+#
+# #Суму негативних чисел
+#     for i in range(NUMS_SIZE):
+#         if numbers[i] < 0:
+#             number.append(numbers[i])
+#             result = sum(number)
+#     print(f"List a negative number: {number}")
+#     print(f"Sum a negative number: {result}")
+#
+# #Суму парних чисел
+#     for i in range(NUMS_SIZE):
+#         if numbers[i] % 2 == 0:
+#             number2.append(numbers[i])
+#             result = sum(number2)
+#         else: #Суму непарних чисел
+#             number3.append(numbers[i])
+#             result2 = sum(number3)
+#     print(f"List a even number: {number2}")
+#     print(f"Sum a even number: {result}")
+#     print(f"List a odd number: {number3}")
+#     print(f"Sum a odd number: {result2}")
+#
+# #Добуток елементів з кратними індексами 3
+# #v1
+#     result = 1
+#     for i in numbers[3::3]:
+#         result *= i
+#     print(f"Index multiple 3 number: {numbers[3::3]}")
+#     print(f"Multiplication a index multiple 3 number: {result}")
+# #v2
+#     result = 1
+#     for i in range(1, len(numbers)):
+#         if i % 3 == 0:
+#             result *= numbers[i]
+#     print(f"Multiplication a index multiple 3 number v2: {result}")
+# #Добуток елементів між мінімальним та максимальним елементом;
+#     result = 1
+#     number4 = []
+#     j = numbers.index(min(numbers))
+#     k = numbers.index(max(numbers))
+#     for i in range(NUMS_SIZE):
+#         if j < k:
+#             number4 = numbers[j:k+1]
+#         else:
+#             number4 = numbers[k:j+1]
+#     for i in range(len(number4)):
+#         result *= number4[i]
+#     print(f"List interval min max: {number4}")
+#     print(f"Multiplying numbers on the interval min max : {result}")
+#
+# #Суму елементів, що знаходяться між першим та останнім позитивними елементами.
+#     result = 0
+#     for i in range(NUMS_SIZE):
+#         try:
+#             if numbers[i] > 0:
+#                 num1 = i
+#                 break
+#         except Exception as error:
+#             print(f"no positive numbers: {error}")
+#     for i in range(NUMS_SIZE - 1, -1, -1):
+#         if numbers[i] > 0:
+#             num2 = i
+#             break
+#     for i in range(num1+1,num2):
+#         result += numbers[i]
+#     print(f"Index the first and last number : {num1}, {num2}")
+#     print(f"sum of numbers in the interval : {result}")
+#
+# except Exception as error:
+#     print(f"Error: {error}")
+
+#2.Є список цілих, заповнений випадковими числами. На підставі даних цього масиву потрібно
 try:
     import random
     NUMS_SIZE = 10
-    MIN_NUMBER = -20
-    MAX_NUMBER = 20
+    MIN_NUMBER = -15
+    MAX_NUMBER = 15
     numbers = []
-    number = []
-    number2 = []
-    number3 = []
     for i in range(NUMS_SIZE):
         numbers.append(random.randint(MIN_NUMBER, MAX_NUMBER))
     print(numbers)
 
-#Суму негативних чисел
-    for i in range(NUMS_SIZE):
-        if numbers[i] < 0:
-            number.append(numbers[i])
-            result = sum(number)
-    print(f"List a negative number: {number}")
-    print(f"Sum a negative number: {result}")
-
-#Суму парних чисел
+#Створити список цілих, що містить лише парні числа з першого списку;
+    number1 = []
+    number2 = []
     for i in range(NUMS_SIZE):
         if numbers[i] % 2 == 0:
-            number2.append(numbers[i])
-            result = sum(number2)
-        else: #Суму непарних чисел
-            number3.append(numbers[i])
-            result2 = sum(number3)
-    print(f"List a even number: {number2}")
-    print(f"Sum a even number: {result}")
-    print(f"List a odd number: {number3}")
-    print(f"Sum a odd number: {result2}")
-
-#Добуток елементів з кратними індексами 3
-#v1
-    result = 1
-    for i in numbers[3::3]:
-        result *= i
-    print(f"Index multiple 3 number: {numbers[3::3]}")
-    print(f"Multiplication a index multiple 3 number: {result}")
-#v2
-    result = 1
-    for i in range(1, len(numbers)):
-        if i % 3 == 0:
-            result *= numbers[i]
-    print(f"Multiplication a index multiple 3 number v2: {result}")
-#Добуток елементів між мінімальним та максимальним елементом;
-    result = 1
-    number4 = []
-    j = numbers.index(min(numbers))
-    k = numbers.index(max(numbers))
-    for i in range(NUMS_SIZE):
-        if j < k:
-            number4 = numbers[j:k+1]
+            number1.append(numbers[i])
+#Створити список цілих, що містить лише непарні числа з першого списку;
         else:
-            number4 = numbers[k:j+1]
-    for i in range(len(number4)):
-        result *= number4[i]
-    print(f"List interval min max: {number4}")
-    print(f"Multiplying numbers on the interval min max : {result}")
+            number2.append(numbers[i])
+    print(f"List a even number: {number1}")
+    print(f"List a odd number: {number2}")
 
-#Суму елементів, що знаходяться між першим та останнім позитивними елементами.
-    result = 0
+#cтворити список цілих, що містить лише негативні числа з першого списку
+    number3 = []
+    number4 = []
     for i in range(NUMS_SIZE):
-        try:
-            if numbers[i] > 0:
-                num1 = i
-                break
-        except Exception as error:
-            print(f"no positive numbers: {error}")
-    for i in range(NUMS_SIZE - 1, -1, -1):
-        if numbers[i] > 0:
-            num2 = i
-            break
-    for i in range(num1+1,num2):
-        result += numbers[i]
-    print(f"Index the first and last number : {num1}, {num2}")
-    print(f"sum of numbers in the interval : {result}")
-
+        if numbers[i] < 0:
+            number3.append(numbers[i])
+#cтворити список цілих, що містить лише позитивні числа з першого списку
+        elif numbers[i] > 0: # что бы ноль не попал
+            number4.append(numbers[i])
+    print(f"List a negative number: {number3}")
+    print(f"List a positive number: {number4}")
 except Exception as error:
     print(f"Error: {error}")
-
-#2Є список цілих, заповнений випадковими числами. На підставі даних цього масиву потрібно
