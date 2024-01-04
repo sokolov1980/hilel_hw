69008f782f69979941cf9ff02b3e6c347c0e8ea4
sokolov1980 <falcon_2005@ukr.net>
2024-01-04 22:01:02 +0200

    implemented task 3

--- dop_hw2.py
+++ dop_hw2.py
@@ -42,16 +42,20 @@ except Exception as error:
 
 
 def calculation_sum(num1, num2):
-    if num1 < num2:
-        return num1
-    return num1 + calculation_sum(num1, num2 - 1)
+    if num1 > num2:
+        return 0
+    return num1 + calculation_sum(num1 + 1, num2)
 
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter first number: "))
-result = calculation_sum(num1, num2)
-print(result)
-
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter first number: "))
+    if num1 >= num2:
+        print(f"Error")
+    else:
+        result = calculation_sum(num1, num2)
+        print(result)
+except Exception as error:
+    print(f"Error: {error}")
 
 #Напишіть рекурсивну функцію, яка приймає одновимірний масив із 100 цілих чисел заповнених випадковим
 # чином і знаходить позицію, з якої починається послідовність із 10 чисел, сума яких мінімальна.
