2e38205b58366b412f6bfe2f317cb455666450bd
sokolov1980 <falcon_2005@ukr.net>
2024-01-15 15:20:29 +0200

    implemented task 3

--- homework9.py
+++ homework9.py
@@ -13,9 +13,13 @@ with open("text10.txt", "r") as source, open("text7dit.txt", "w") as destination
 
 # 2.Даний текстовий файл. Підрахувати кількість слів у ньому.
 with open("text10.txt", "r") as source:
-    word_count = 0
-    for word in words.split():
-        word_count += len(re.findall(r'^([A-Za-z]+[.,!?]?){1,}$', word))
+    # v 1
+    # word_count = 0
+    # for word in words.split():
+    #     word_count += len(re.findall(r'^([A-Za-z]+[.,!?]?){1,}$', word))
+    # v 2
+    text = source.read()
+    word_count = len(text.split(' '))
     print(f"Word count: {word_count}")
 
 # 3.Створіть програму, яка перевіряє текст на неприпустимі слова.
@@ -23,13 +27,9 @@ with open("text10.txt", "r") as source:
 # За підсумками роботи програми необхідно показати статистику дій.
 
 with open("text11.txt", "r") as source, open("text11_new.txt", "w") as destination:
-    count = 0
     text = source.read()
     invalid_word = input("Enter invalid word: ")
-    length = len(invalid_word)
-    for word in text:
-        text_new = text.replace(invalid_word, '*'*length)
-        count = text.count(invalid_word)
-        destination.write(text_new)
-        break
+    text_new = text.replace(invalid_word, '*' * len(invalid_word))
+    count = text.count(invalid_word)
+    destination.write(text_new)
     print(f"Invalid count: {count}")
