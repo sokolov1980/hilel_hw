ef3f6fa43b234a20ecc47c8d5de6858d7b29b40b
sokolov1980 <falcon_2005@ukr.net>
2024-01-18 20:47:42 +0200

    implemented task 1

--- homework10.py
+++ homework10.py
@@ -4,34 +4,55 @@
 
 class City:
     __city_name = "no name"
-    __region =
-    __country =
-    __city_residents =
-    __postcode =
-    __telephone_code =
+    __region = "no region name"
+    __country = "no country"
+    __city_residents = 1000
+    __postcode = "no index"
+    __telephone_code = "no code"
 
-    def __init__(self, name, age, hobby="no info"):
-        self.name = name  # name setter
-        self.age = age  # age setter
-        self.hobby = hobby  # public field
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        if 2 < len(name) < 30:
-            self.__name = name
+    def __init__(self, c_name, region, country, city_res, postcode, tel_code):
+        self.c_name = c_name
+        self.region = region
+        self.country = country
+        self.city_res = city_res
+        self.postcode = postcode
+        self.tel_code = tel_code
 
     @property
-    def age(self):
-        return self.__age
+    def c_name(self):
+        return self.__city_name
 
-    @age.setter
-    def age(self, age):
-        if 0 < age < 150:
-            self.__age = age
+    @c_name.setter
+    def c_name(self, c_name):
+        if 1 < len(c_name) < 100:
+            self.__city_name = c_name
+
+    @property
+    def region(self):
+        return self.__region
+
+    @region.setter
+    def region(self, region):
+        if 4 < len(region) < 100:
+            self.__region = region
+
+    @property
+    def country(self):
+        return self.__country
+
+    @country.setter
+    def country(self, country):
+        if 4 < len(country) < 60:
+            self.__country = country
+
+    @property
+    def city_res(self):
+        return self.__city_residents
+
+    @city_res.setter
+    def city_res(self, city_res):
+        if 500 < city_res < 38000000:
+            self.__city_residents = city_res
 
     def show_info(self):
         print(f"Name: {self.__name}\nAge: {self.__age}\nHobby: {self.hobby}")
