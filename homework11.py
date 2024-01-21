89f58343faf89514be7470a36ebaac9848f2af28
sokolov1980 <falcon_2005@ukr.net>
2024-01-21 22:25:45 +0200

    implemented task 1

--- homework11.py
+++ homework11.py
@@ -2,7 +2,7 @@
 # Зразковий список класів: Person, Teacher, Student, Subject, Academy і т.д.
 # Продумати архітектуру: реалізувати принципи ООП
 
-class Academy:
+class Academy(object):
     __name_academy = "Academy"
 
     def __init__(self, name_academy):
@@ -63,40 +63,6 @@ class Subject(Faculty):
         print(f"Subject: {self.subject}")
 
 
-class Student(Faculty):
-    __name_student = "John Dou"
-    __age = "no 18 years old"
-    __education = "secondary"
-
-    def __init__(self, name_academy, name_faculty, name_student, age=None, education=None):
-        super().__init__(name_academy, name_faculty)
-        self.__name_student = name_student
-        self.__age = age
-        self.__education = education
-
-    @property
-    def name_student(self):
-        return self.__name_student
-
-    @name_student.setter
-    def name_student(self, name_student):
-        if 10 < len(name_student) < 60:
-            self.__name_student = name_student
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age):
-        if 18 <= age:
-            self.__age = age
-
-    def show_info(self):
-        super().show_info()
-        print(f"Name student: {self.name_student}, Age: {self.age}, Education: {self.education}")
-
-
 class Teacher(Subject):
     __name_teacher = "John Dou"
     __age_t = "no 24 years old"
@@ -126,19 +92,62 @@ class Teacher(Subject):
         if 24 <= age_t:
             self.__age_t = age_t
 
+    @property
+    def academic_degree(self):
+        return self.__academic_degree
+
     def show_info(self):
         super().show_info()
         print(f"Name teacher: {self.name_teacher}, Age: {self.age_t}, Academic degree: {self.academic_degree}")
 
 
-class Person(Student, Teacher):
+class Student(Faculty):
+    __name_student = "John Dou"
+    __age = "no 18 years old"
+    __education = "secondary"
+
+    def __init__(self, name_academy, name_faculty, name_student=None, age=None, education=None):
+        Faculty.__init__(self, name_academy, name_faculty)
+        # super().__init__(name_academy, name_faculty)
+        self.__name_student = name_student
+        self.__age = age
+        self.__education = education
+
+    @property
+    def name_student(self):
+        return self.__name_student
+
+    @name_student.setter
+    def name_student(self, name_student):
+        if 10 < len(name_student) < 60:
+            self.__name_student = name_student
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if 18 <= age:
+            self.__age = age
+
+    @property
+    def education(self):
+        return self.__education
+
+    def show_info(self):
+        super().show_info()
+        print(f"Name student: {self.name_student}, Age: {self.age}, Education: {self.education}")
+
+
+class Person(Student, Teacher):  # участник учебной группы
     __name_group = "some"
     __quantity = "no data"
 
-    def __init__(self, name_academy, name_faculty, name_person, sex, age, education, subject, age_t,
+    def __init__(self, name_academy, name_faculty, name_student, age, education, subject, name_teacher, age_t,
                  academic_degree, name_group=None, quantity=None):
-        Student.__init__(self, name_academy, name_faculty, name_person, sex, age, education)
-        Teacher.__init__(self, name_academy, name_faculty, name_person, sex, subject, age_t, academic_degree)
+        Student.__init__(self, name_academy, name_faculty, name_student, age, education)
+        Teacher.__init__(self, name_academy, name_faculty, subject, name_teacher, age_t, academic_degree)
         self.__name_group = name_group
         self.__quantity = quantity
 
@@ -164,11 +173,15 @@ class Person(Student, Teacher):
         super().show_info()
         print(f"Name group: {self.name_group}, Quantity: {self.quantity}")
 
-
-first_group = Group("Hogwarts", "Gryffindor", "Garry Potter", "Male", 19,
-                    "secondary", "Severus Snape", "Male", "potions making", 35,
-                    "doctor")
-first_group.show_info()
-print(Group.mro())
-
-#name_academy, name_faculty, name_person, sex, age, education, age_t, academic_degree, name_group, quantity
+try:
+    first_group: list[Person] = [Person("Hogwarts", "Gryffindor", "Garry Potter", 19, "secondary",
+                        "Potions making", "Severus Snape", 35, "doctor", "GD-21",
+                        25), Person("Hogwarts", "Gryffindor", "Ron Weasley", 19, "secondary",
+                        "Divination", "Sybill Trelawney", 40, "doctor", "GD-21",
+                        25), Person("", "", "Dobby", 150, "",
+                        "", "", "", "", "", "")]
+    for person in first_group:
+        person.show_info()
+    print(Person.mro())
+except Exception as error:
+    print(error)
